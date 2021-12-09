369a17237d49b32caf72f03d61fb20f5c172f427
oralabuser <82149355+oralabuser@users.noreply.github.com>
2021-12-09 16:49:51 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -2,8 +2,8 @@
 export PATH=$PATH:/usr/local/bin/terraform
 echo $PATH
 
-ln -s /usr/local/bin/terraform terraform
 cd /home/ec2-user/terraform_lab
+ln -s /usr/local/bin/terraform terraform
 
 pwd
 
